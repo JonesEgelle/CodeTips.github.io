75d37e418c9db09b224c785370d10e67dc0bd4d7
C&Jay♪ <157700657+JonesEgelle@users.noreply.github.com>
2024-08-21 17:47:11 +0100

    Update codeTips.js (21st Aug 2024)
    1. Update to the toggleDropdown, toggleMobileDropdown, openMenu, closeMenu (*function)

--- codeTips.js
+++ codeTips.js
@@ -1,13 +1,18 @@
-const sidemenu = document.querySelector("#sidemenu");
-
-function openMenu() {
-  sidemenu.style.transform = "translateX(-16rem)";
-}
-function closeMenu() {
-  sidemenu.style.transform = "translateX(16rem)";
-}
-
 function toggleDropdown() {
-  let dropdown = document.querySelector("#dropDownButton #dropdown ");
+  const dropdown = document.getElementById("dropdown");
   dropdown.classList.toggle("hidden");
+}
+
+function toggleMobileDropdown() {
+  const dropdown = document.getElementById("mobileDropdown");
+  dropdown.classList.toggle("hidden");
+}
+function openMenu() {
+  document.getElementById("sidemenu").classList.remove("right-[-256px]");
+  document.getElementById("sidemenu").classList.add("right-0");
+}
+
+function closeMenu() {
+  document.getElementById("sidemenu").classList.remove("right-0");
+  document.getElementById("sidemenu").classList.add("right-[-256px]");
 }
